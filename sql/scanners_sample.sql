41b549ab44e2458890d6786625b480253497eb67
punker85 <djtrast@gmail.com>
2019-10-06 10:56:09 -0400

    Sample data to represent our Raspberry Pi devices

---
+++ sql/scanners_sample.sql
@@ -0,0 +1,3 @@
+insert into scanner values (0, 'Daniel', 'RPi 4 Model B') ;
+insert into scanner values (1, 'Kolby', 'RPi 3 Model B+') ;
+insert into scanner values (2, 'Kassandra', 'RPi 3 Model B') ;
