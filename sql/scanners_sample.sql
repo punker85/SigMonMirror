b0edc2eed04f970a58da1e38ff02118689902116
punker85 <djtrast@gmail.com>
2019-11-16 18:35:00 -0500

    Added our device BD_ADDR's to scanner table

--- sql/scanners_sample.sql
+++ sql/scanners_sample.sql
@@ -1,3 +1,4 @@
-insert into scanner values (0, 'Daniel', 'RPi 4 Model B') ;
-insert into scanner values (1, 'Kolby', 'RPi 3 Model B+') ;
-insert into scanner values (2, 'Kassandra', 'RPi 3 Model B') ;
+use sigmon;
+insert into scanner values (0, 'Daniel', 'RPi 4 Model B', 'DC:A6:32:1C:20:43') ;
+insert into scanner values (1, 'Kolby', 'RPi 3 Model B+', 'B8:27:EB:56:AB:B1') ;
+insert into scanner values (2, 'Kassandra', 'RPi 3 Model B+', 'B8:27:EB:40:00:44') ;
