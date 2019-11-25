4cc1be88a8c4c604d2d8deb251c1c033d1351cc5
punker85 <djtrast@gmail.com>
2019-11-24 20:04:53 -0500

    Tables for distance/GPS calculations and Triplet

--- sql/schema.sql
+++ sql/schema.sql
@@ -44,3 +44,48 @@ create table if not exists rssi (
 		references device(id)
         on delete cascade
 ) ;
+
+create table if not exists triplet (
+	id int unsigned not null auto_increment comment 'Identifier',
+    exp1 mediumint unsigned comment 'Experiment 1',
+    exp2 mediumint unsigned comment 'Experiment 2',
+    exp3 mediumint unsigned comment 'Experiment 3',
+    primary key(id),
+    constraint single_triple unique (exp1, exp2, exp3),
+    foreign key(exp1)
+	references experiment(id)
+        on delete set null,
+    foreign key(exp2)
+	references experiment(id)
+        on delete set null,
+    foreign key(exp3)
+	references experiment(id)
+        on delete set null
+) ;
+
+create table if not exists trilat (
+	id int unsigned not null auto_increment comment 'Identifier',
+    triple int unsigned not null default '0' comment 'Triple Experiment',
+    mac char(17) not null default 'FF:FF:FF:FF:FF:FF' comment 'MAC address',
+    time time(0) not null default '00:00:00' comment 'Time',
+    lat decimal(13,10) not null default '0.0' comment 'Latitude',
+    lng decimal(13,10) not null default '0.0' comment 'Longitude',
+    primary key(id),
+    constraint mac_per_time unique (triple, mac, time),
+    foreign key(triple)
+	references triplet(id)
+        on delete no action
+) ;
+
+create table if not exists dist (
+	id int unsigned not null auto_increment comment 'Identifier',
+    experiment mediumint unsigned not null default '0' comment 'Experiment',
+    mac char(17) not null default 'FF:FF:FF:FF:FF:FF' comment 'MAC address',
+    start_time datetime(0) not null default '00:00:00' comment 'Time',
+    distance decimal(5,2) not null default '0.0' comment 'Distance in meters',
+    primary key(id),
+    constraint mac_exp_at_time unique (experiment, mac, start_time),
+    foreign key(experiment)
+	references experiment(id)
+        on delete cascade
+) ;
