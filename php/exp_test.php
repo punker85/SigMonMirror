63281234d771cb6f6e06eadf4bef9cf8525cf104
punker85 <djtrast@gmail.com>
2019-11-24 20:07:16 -0500

    Function for returning JS array of statistics data

--- php/exp_test.php
+++ php/exp_test.php
@@ -1,4 +1,94 @@
 <?php
+function tab_content_exp(mysqli &$conn, string $database, int $experiment) {
+	$params = "{";
+	$samples = 0;
+	$query = "SELECT location FROM " .$database. ".experiment WHERE id = " .$experiment;
+	if(!($rslt = $conn->query($query))) {
+		echo "<script>output.add(\"MySQL Error: " .$conn->error. "\");" 
+			."output.timestamp() ;</script>";
+	} else {
+		$params .= "name: \"" .$rslt->fetch_assoc()["location"]. "\",";
+	}
+	$rslt->free();
+	$query = "SELECT COUNT(*) as num FROM " .$database. ".device WHERE experiment = " .$experiment;
+	if(!($rslt = $conn->query($query))) {
+		echo "<script>output.add(\"MySQL Error: " .$conn->error. "\");" 
+			."output.timestamp() ;</script>";
+	} else {
+		$params .= "devs: \"" .$rslt->fetch_assoc()["num"]. "\",";
+	}
+	$rslt->free();
+	$query = "select count(*) as sum from " .$database. ".experiment a inner join device b on a.id = b.experiment inner join rssi c on b.id = c.device where a.id = " .$experiment;
+	if(!($rslt = $conn->query($query))) {
+		echo "<script>output.add(\"MySQL Error: " .$conn->error. "\");" 
+			."output.timestamp() ;</script>";
+	} else {
+		$samples = $rslt->fetch_assoc()["sum"];
+		$params .= "samps: \"" .$samples. "\",";
+	}
+	$rslt->free();
+	$query = "select TIMEDIFF(MAX(c.scan_update), MIN(c.scan_update)) AS diff from " .$database. ".experiment a inner join device b on a.id = b.experiment inner join rssi c on b.id = c.device where a.id = " .$experiment;
+	if(!($rslt = $conn->query($query))) {
+		echo "<script>output.add(\"MySQL Error: " .$conn->error. "\");" 
+			."output.timestamp() ;</script>";
+	} else {
+		$params .= "time: \"" .$rslt->fetch_assoc()["diff"]. "\",";
+	}
+	$rslt->free();
+	$query = "select TIME_TO_SEC(TIMEDIFF(MAX(c.scan_update), MIN(c.scan_update))) AS secs from " .$database. ".experiment a inner join device b on a.id = b.experiment inner join rssi c on b.id = c.device where a.id = " .$experiment;
+	if(!($rslt = $conn->query($query))) {
+		echo "<script>output.add(\"MySQL Error: " .$conn->error. "\");" 
+			."output.timestamp() ;</script>";
+	} else {
+		$secs = $rslt->fetch_assoc()["secs"];
+		if($secs == 0)
+			$secs = 1;
+		$params .= "sps: \"" .round($samples/$secs, 3). "\",";
+	}
+	$rslt->free();
+	$query = "select MIN(c.rssi) AS low, MAX(c.rssi) AS high from " .$database. ".experiment a inner join device b on a.id = b.experiment inner join rssi c on b.id = c.device where a.id = " .$experiment. " and c.rssi < 0";
+	if(!($rslt = $conn->query($query))) {
+		echo "<script>output.add(\"MySQL Error: " .$conn->error. "\");" 
+			."output.timestamp() ;</script>";
+	} else {
+		$temp = $rslt->fetch_assoc();
+		$params .= "low: \"" .$temp["low"]. "\",";
+		$params .= "high: \"" .$temp["high"]. "\",";
+	}
+	$rslt->free();
+	$query = "select AVG(c.rssi) AS mean from " .$database. ".experiment a inner join device b on a.id = b.experiment inner join rssi c on b.id = c.device where a.id = " .$experiment. " and c.rssi < 0";
+	if(!($rslt = $conn->query($query))) {
+		echo "<script>output.add(\"MySQL Error: " .$conn->error. "\");" 
+			."output.timestamp() ;</script>";
+	} else {
+		$params .= "mean: \"" .round($rslt->fetch_assoc()["mean"], 2). "\",";
+	}
+	$rslt->free();
+	
+	$rank = 0;
+	$query = "select floor(count(*)/2) from " .$database. ".experiment a inner join device b on a.id = b.experiment inner join rssi c on b.id = c.device "
+		."where a.id = " .$experiment. " and c.rssi < 0";
+	if(!($rslt = $conn->query($query))) {
+		echo "<script>output.add(\"MySQL Error: " .$conn->error. "\");" 
+			."output.timestamp() ;</script>";
+	} else {
+		$rank = $rslt->fetch_row()[0];
+		$rslt->free();
+		$query = "select c.rssi from " .$database. ".experiment a inner join device b on a.id = b.experiment inner join rssi c on b.id = c.device "
+		."where a.id = " .$experiment. " and c.rssi < 0 order by c.rssi asc limit " .$rank. ",1";
+		if(!($rslt = $conn->query($query))) {
+			echo "<script>output.add(\"MySQL Error: " .$conn->error. "\");" 
+				."output.timestamp() ;</script>";
+		} else {
+			$median = $rslt->fetch_row()[0];
+			$params .= "median: \"" .$median. "\"";
+		}
+	}
+	$rslt->free();
+	$params .= "}";
+    return $params;
+}
+
 try {
 	$access = new DOMDocument();
 	$access->load("../../access.xml");
@@ -83,49 +173,159 @@ try {
 				."testmenu.resetExp();" ;
 		}
 	} elseif(isset($_POST["all"])) {
+		echo "<ul class=\"list-group\">"; 
+		$tri_exps = array();
+		$qstr = "SELECT exp1, exp2, exp3, a.location as aloc, a.lat as alat, a.lng as alng, b.location as bloc, b.lat as blat, b.lng as blng, c.location as cloc, c.lat as clat, c.lng as clng FROM " .$db. ".triplet"
+			." inner join " .$table. " a on triplet.exp1 = a.id"
+			." inner join " .$table. " b on triplet.exp2 = b.id"
+			." inner join " .$table. " c on triplet.exp3 = c.id";
+		if(!($result = $mysqli->query($qstr))) {
+			echo "<script>output.add(\"MySQL Error: " .$mysqli->error. "\");" 
+				."output.timestamp() ;</script>";
+		} else {
+			$j = 0;
+			while ($row = $result->fetch_assoc()) {
+				echo "<li class=\"list-group-item list-group-flush list-group-item-action\"><ul style=\"list-style-type: none; padding: 0\">" 
+					."<li value=\"" .$row["exp1"]. "\" disabled>" .$row["aloc"]. "</li>"
+					."<li value=\"" .$row["exp2"]. "\" disabled>" .$row["bloc"]. "</li>"
+					."<li value=\"" .$row["exp3"]. "\" disabled>" .$row["cloc"]. "</li>"				
+				."</ul></li>";
+				echo "<script>nodes[" .$row["exp1"]. "] = new Node(map," .$row["exp1"]. ",\"" .$row["aloc"]. "\",\"" .$row["alat"]. "\",\"" .$row["alng"]. "\"," .tab_content_exp($mysqli, $db, $row["exp1"]). ");</script>" ;
+				echo "<script>nodes[" .$row["exp2"]. "] = new Node(map," .$row["exp2"]. ",\"" .$row["bloc"]. "\",\"" .$row["blat"]. "\",\"" .$row["blng"]. "\"," .tab_content_exp($mysqli, $db, $row["exp2"]). ");</script>" ;
+				echo "<script>nodes[" .$row["exp3"]. "] = new Node(map," .$row["exp3"]. ",\"" .$row["cloc"]. "\",\"" .$row["clat"]. "\",\"" .$row["clng"]. "\"," .tab_content_exp($mysqli, $db, $row["exp3"]). ");</script>" ;
+				array_push($tri_exps, $row["exp1"]);
+				array_push($tri_exps, $row["exp2"]);
+				array_push($tri_exps, $row["exp3"]);
+				$j++ ;
+			}
+			$result->free();
+			
+			echo "<script>output.add(\"" .$j. " Tri-Nodes loaded.\");</script>" ;
+			echo "<script>$(\"#collapseExp div ul li.list-group-item:has(ul)\").hover( function() { $(this).addClass(\"list-group-item-danger\").removeClass(\"\"); },"
+					."function() { $(this).addClass(\"\").removeClass(\"list-group-item-danger\"); })"
+					.".click( function() {"
+					."let item = $(this);"
+					."item.addClass(\"disabled\");"
+					."let triple = item.find(\"li\");"
+					."let node1 = nodes[triple.eq(0).val()];"
+					."let node2 = nodes[triple.eq(1).val()];"
+					."let node3 = nodes[triple.eq(2).val()];"
+					."node1.draw(false);"
+					."node2.draw(false);"
+					."node3.draw(false);"
+					."let lat = (parseFloat(node1.latitude) + parseFloat(node2.latitude) + parseFloat(node3.latitude)) / 3;"
+					."let lng = (parseFloat(node1.longitude) + parseFloat(node2.longitude) + parseFloat(node3.longitude)) / 3;"
+					."setTimeout(function(){map.panTo(new google.maps.LatLng(lat, lng));}, 500);"
+					."setTimeout(function(){item.removeClass(\"disabled\");}, 1300);"
+					."if(!$(this).hasClass(\"list-group-item-success\")) {"
+						."$(this).removeClass(\"list-group-item-danger\").off(\"mouseenter mouseleave\").addClass(\"list-group-item-success\");"
+						."node1.listener = node1.marker.addListener(\"click\", function(e) {"
+							."output.push();output.add(\"Loading Devices:\");"
+							."setTimeout(function(){map.panTo(node1.marker.position);}, 100);"
+							."setTimeout(function(){map.setZoom(20);}, 400);"
+							."cpanel.add(\"experiment\", node1.params);"
+							."cpanel.select(\"experiment\");"
+							."var formData = {\"id\" : node1.id};"
+							."$.ajax({url : \"./php/dev_test.php\","
+								."type: \"POST\","
+								."data: formData,"
+								."dataType: \"html\","
+								."success: function (data) {"
+									."$(\"#collapseDev div\").html(data);},"
+								."error: function (jXHR, textStatus, errorThrown) {"
+									."alert(errorThrown);}"
+						."});});"
+						."node2.listener = node2.marker.addListener(\"click\", function(e) {"
+							."output.push();output.add(\"Loading Devices:\");"
+							."setTimeout(function(){map.panTo(node2.marker.position);}, 100);"
+							."setTimeout(function(){map.setZoom(20);}, 400);"
+							."cpanel.add(\"experiment\", node2.params);"
+							."cpanel.select(\"experiment\");"
+							."var formData = {\"id\" : node2.id};"
+							."$.ajax({url : \"./php/dev_test.php\","
+								."type: \"POST\","
+								."data: formData,"
+								."dataType: \"html\","
+								."success: function (data) {"
+									."$(\"#collapseDev div\").html(data);},"
+								."error: function (jXHR, textStatus, errorThrown) {"
+									."alert(errorThrown);}"
+						."});});"
+						."node3.listener = node3.marker.addListener(\"click\", function(e) {"
+							."output.push();output.add(\"Loading Devices:\");"
+							."setTimeout(function(){map.panTo(node3.marker.position);}, 100);"
+							."setTimeout(function(){map.setZoom(20);}, 400);"
+							."cpanel.add(\"experiment\", node3.params);"
+							."cpanel.select(\"experiment\");"
+							."var formData = {\"id\" : node3.id};"
+							."$.ajax({url : \"./php/dev_test.php\","
+								."type: \"POST\","
+								."data: formData,"
+								."dataType: \"html\","
+								."success: function (data) {"
+									."$(\"#collapseDev div\").html(data);},"
+								."error: function (jXHR, textStatus, errorThrown) {"
+									."alert(errorThrown);}"
+						."});});"
+					."} else {"
+						."$(this).removeClass(\"list-group-item-success\").addClass(\"list-group-item-danger\")"
+						.".hover( function() { $(this).addClass(\"list-group-item-danger\").removeClass(\"\"); },"
+						."function() { $(this).addClass(\"\").removeClass(\"list-group-item-danger\"); });"
+						."node1.listener.remove();"
+						."node2.listener.remove();"
+						."node3.listener.remove();"
+					."}});</script>";
+		}
+		
 		$qstr = "SELECT * FROM " .$db. "." .$table;
 		if(!($result = $mysqli->query($qstr))) {
 			echo "<script>output.add(\"MySQL Error: " .$mysqli->error. "\");" 
 				."output.timestamp() ;</script>";
 		} else {
 			$i = 0;
-			echo "<ul class=\"list-group\">";
 			while ($row = $result->fetch_assoc()) {
-				echo "<li class=\"list-group-item list-group-flush list-group-item-action\" value=\"" .$i. "\">" .$row["location"]. "</li>";
-				echo "<script>nodes.add(new Node(map," .$row["id"]. ",\"" .$row["location"]. "\",\"" .$row["lat"]. "\",\"" .$row["lng"]. "\"));</script>" ;
-				$i++ ;
+				if(!in_array($row["id"], $tri_exps)) {
+					echo "<li class=\"list-group-item list-group-flush list-group-item-action\" value=\"" .$row["id"]. "\">" .$row["location"]. "</li>";
+					echo "<script>nodes[" .$row["id"]. "] = new Node(map," .$row["id"]. ",\"" .$row["location"]. "\",\"" .$row["lat"]. "\",\"" .$row["lng"]. "\"," .tab_content_exp($mysqli, $db, $row["id"]). ");</script>" ;
+					$i++ ;
+				}
 			}
 			echo "</ul>";
-			echo "<script>output.add(\"" .$i. " experiments loaded.\");" 
+			echo "<script>output.add(\"" .$i. " single experiments loaded.\");" 
 				."output.timestamp();</script>" ;
-			echo "<script>$(\"#collapseExp div ul li\").hover( function() { $(this).addClass(\"list-group-item-danger\").removeClass(\"\"); },"
+			echo "<script>$(\"#collapseExp div ul li.list-group-item:not(:has(ul))\").hover( function() { $(this).addClass(\"list-group-item-danger\").removeClass(\"\"); },"
 					."function() { $(this).addClass(\"\").removeClass(\"list-group-item-danger\"); })"
 					.".click( function() {"
 					."let item = $(this);"
 					."item.addClass(\"disabled\");"
-					."let node = nodes.get(item.val());"
+					."let node = nodes[item.val()];"
 					."node.draw();"
-					."node.marker.addListener(\"click\", function(e) {"
-						."output.push();output.add(\"Loading Devices:\");"
-						."var formData = {\"id\" : node.id};"
-						."$.ajax({url : \"./php/dev_test.php\","
-							."type: \"POST\","
-							."data: formData,"
-							."dataType: \"html\","
-							."success: function (data) {"
-								."$(\"#collapseDev div\").html(data);},"
-							."error: function (jXHR, textStatus, errorThrown) {"
-								."alert(errorThrown);}"
-					."});});"
 					."setTimeout(function(){item.removeClass(\"disabled\");}, 1300);"
 					."if(!$(this).hasClass(\"list-group-item-success\")) {"
-						."$(this).removeClass(\"list-group-item-danger\").off(\"mouseenter mouseleave\").addClass(\"list-group-item-success\"); "
+						."$(this).removeClass(\"list-group-item-danger\").off(\"mouseenter mouseleave\").addClass(\"list-group-item-success\");"
+						."node.listener = node.marker.addListener(\"click\", function(e) {"
+							."output.push();output.add(\"Loading Devices:\");"
+							."setTimeout(function(){map.panTo(node.marker.position);}, 100);"
+							."setTimeout(function(){map.setZoom(20);}, 400);"
+							."cpanel.add(\"experiment\", node.params);"
+							."cpanel.select(\"experiment\");"
+							."var formData = {\"id\" : node.id};"
+							."$.ajax({url : \"./php/dev_test.php\","
+								."type: \"POST\","
+								."data: formData,"
+								."dataType: \"html\","
+								."success: function (data) {"
+									."$(\"#collapseDev div\").html(data);},"
+								."error: function (jXHR, textStatus, errorThrown) {"
+									."alert(errorThrown);}"
+						."});});"
 					."} else {"
 						."$(this).removeClass(\"list-group-item-success\").addClass(\"list-group-item-danger\")"
 						.".hover( function() { $(this).addClass(\"list-group-item-danger\").removeClass(\"\"); },"
 						."function() { $(this).addClass(\"\").removeClass(\"list-group-item-danger\"); });"
+						."node.listener.remove();"
 					."}});</script>";
-			echo "<script>$(\"#showExp\").html(\"<span>Experiments</span><span class=\\\"badge badge-pill badge-light float-right\\\" style=\\\"margin-top: 0.25rem\\\">" .$i. "</span>\");</script>";
+			echo "<script>$(\"#showExp\").html(\"<span>Experiments</span><span class=\\\"badge badge-pill badge-light float-right\\\" style=\\\"margin-top: 0.25rem\\\">" .($i+$j). "</span>\");</script>";
 		}
 		$result->free() ;
 	}
